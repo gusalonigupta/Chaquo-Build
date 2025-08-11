5bc700e0983ded87b95f472c825f8bae969713b7
nope <nope>
2025-08-12 04:11:14 +0530

    Test AAR [Fix 2]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.chaquo.python") version "16.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
+    id("com.android.library") version "8.4.1" apply false
 }
 
 allprojects {
